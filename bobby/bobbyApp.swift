77126dc6f185bfb2ebed16421e65f9cf650279f8
Anantika <anantika.tiwari09@rafflesgirlssch.edu.sg>
2025-08-02 14:27:25 +0800

    Initial Commit

---
+++ bobby/bobbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  bobbyApp.swift
+//  bobby
+//
+//  Created by Anantika Tiwari on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct bobbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
