b85bcd1a0cf155763683f4f4bf30b82a87621fbd
Anantika <anantika.tiwari09@rafflesgirlssch.edu.sg>
2025-08-02 14:50:11 +0800

    adjust positions

--- bobby/ContentView.swift
+++ bobby/ContentView.swift
@@ -22,9 +22,9 @@ struct ContentView: View {
             EmojiView(symbol: "🩲", size: 50, x: 0,  y: 0)
             EmojiView(symbol: "🖋️", size: 50, x: 60,  y: -60)
             EmojiView(symbol: "🍕", size: 50, x: -60,  y: -60)
-            EmojiView(symbol: "🎀", size: 50, x: 0,  y: -160)
-            EmojiView(symbol: "🏳️‍🌈", size: 10, x: -11,  y: -140)
-            EmojiView(symbol: "🏳️‍🌈", size: 10, x: 11,  y: -140)
+            EmojiView(symbol: "🎀", size: 50, x: 0,  y: -165)
+            EmojiView(symbol: "🏳️‍🌈", size: 10, x: -11,  y: -135)
+            EmojiView(symbol: "🏳️‍🌈", size: 10, x: 11,  y: -135)
 
             EmojiView(symbol: "👢", size: 50, x: 38,  y: 60)
             EmojiView(symbol: "👢", size: 70, x: -26,  y: 60)
